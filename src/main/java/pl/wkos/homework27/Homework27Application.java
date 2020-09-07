b37e0f4b8ae4525222a958a8265efe1dd8d71c26
Wojciech Kos <obohp@wp.pl>
2020-09-07 20:25:45 +0200

    Initial commit

---
+++ src/main/java/pl/wkos/homework27/Homework27Application.java
@@ -0,0 +1,13 @@
+package pl.wkos.homework27;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework27Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Homework27Application.class, args);
+    }
+
+}
